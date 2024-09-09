0d070672a330a8dab83412d98d08ac7cbb118e9c
FORMATION <FORMATION@LAPTOP-H6GDXD3>
2024-09-09 13:20:27 +0200

    09/09/24 13h20

---
+++ Serie I/Ex1_ElementaryOperations.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie_I
+{
+    public static class ElementaryOperations
+    {
+        public static void BasicOperation(int a, int b, char operation)
+        {
+            int calcul = 0;
+            string result;
+            if (b == 0)
+            {
+                result = "Opération invalide.";
+            }        
+            else
+            {
+                switch (operation)
+                {
+                    case '+' :
+                        calcul = a + b;
+                        result = calcul.ToString();
+                        break;
+
+                    case '-' :
+                        calcul = a - b;
+                        result = calcul.ToString();
+                        break;
+
+                    case '*' :
+                        calcul = a * b;
+                        result = calcul.ToString();
+                        break;  
+                        
+                    case '/' :
+                        calcul = a / b;
+                        result = calcul.ToString();
+                        break;
+
+                    default :
+                        result = "Opération invalide.";
+                    break;
+                }
+            }
+            Console.WriteLine($"{a} {operation} {b} = {result}");
+        }
+
+    public static void IntegerDivision(int a, int b)
+        {
+            int q = 0;
+            int r = 0;
+
+            if (b == 0)
+            {
+                Console.WriteLine($"{a} : {b} = Opération invalide");
+            }
+            else
+            {
+                q = a / b;
+                r = a % b;
+                switch (r)
+                {
+                    case 0:
+                    Console.WriteLine($"{a} = {q} * {b}");
+                    break;
+
+                    default:
+                    Console.WriteLine($"{a} = {q} * {b} + {r}");
+                    break;
+                }
+            }
+        }
+
+    public static void Pow(int a, int b)
+        {
+        int p = a;
+        if (b < 0)
+            {
+                Console.WriteLine($"{a} ^ {b} = Opération invalide");
+            }
+        else if (b == 0)
+            {
+                p = 1;
+                Console.WriteLine($"{a} ^ {b} = {p}");
+            }
+        else
+            {
+                for (int i = 1 ; i < b ; i++)
+                {
+                    p = p * a;
+                }
+                Console.WriteLine($"{a} ^ {b} = {p}");
+            }
+        }
+    }
+}
---
+++ Serie I/Ex2_SpeakingClock.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie_I
+{
+    public static class SpeakingClock
+    {
+        public static string GoodDay(int heure)
+        {
+            string message;
+            switch (heure)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                    message = "Merveilleuse nuit !";
+                break;
+
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                    message = "Bonne matinée !";
+                break;
+
+                case 12:
+                    message = "Bon appétit !";
+                break;
+
+                case 13:
+                case 14:
+                case 15:
+                case 16:
+                case 17:
+                case 18:
+                    message = "Profitez de votre après-midi !";
+                break;
+
+                case 19:
+                case 20:
+                case 21:
+                case 22:
+                case 23:
+                    message = "Passez une bonne soirée !";
+                break;
+
+                default:
+                    message = "heure non valide";
+                break;
+
+            }
+            Console.WriteLine($"Il est {heure} h, {message}");
+            return string.Empty;
+        }
+    }
+}

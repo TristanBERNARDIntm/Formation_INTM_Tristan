2ad7881f70a1c63b9d1a9ae7b6068f20a5dff2df
FORMATION <FORMATION@LAPTOP-H6GDXD3>
2024-09-18 11:08:30 +0200

    PROJET I

--- Projet I/Program.cs
+++ Projet I/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,61 +13,186 @@ namespace Projet_I
     {
         static void Main(string[] args)
         {
-            string comptes = @"C:\Users\FORMATION\source\repos\Formation_INTM_Tristan\Projet I\bin\Debug\Comptes.csv";
-            string transactions = @"C:\Users\FORMATION\source\repos\Formation_INTM_Tristan\Projet I\bin\Debug\Transactions.csv";
-            string status = @"C:\Users\FORMATION\source\repos\Formation_INTM_Tristan\Projet I\bin\Debug\StatutsTransactions.csv";
+            string fcomptes = @"C:\Users\FORMATION\source\repos\Formation_INTM_Tristan\Projet I\bin\Debug\Comptes.csv";
+            string ftransactions = @"C:\Users\FORMATION\source\repos\Formation_INTM_Tristan\Projet I\bin\Debug\Transactions.csv";
+            string fstatuts = @"C:\Users\FORMATION\source\repos\Formation_INTM_Tristan\Projet I\bin\Debug\StatutsTransactions.csv";
 
-            Console.WriteLine("Fichier Comptes");
-            using (StreamReader sr = new StreamReader(comptes))
+            // recupération du fichier comptes dans une liste d'objet
+            List<Comptes> cpts = new List<Comptes>();
+            using (StreamReader sr = new StreamReader(fcomptes))
             {
-                string line = sr.ReadLine();
+                string ligne = sr.ReadLine();
                 string[] val;
-                Dictionary<int,int> cpts = new Dictionary<int,int>();
-                
-                while (line != null)
+
+                while (ligne != null)
                 {
-                  //  Console.WriteLine(line);
+                    val = ligne.Split(';');               
+
+                    bool entierNum = int.TryParse(val[0], out int NumCpt);
+                    bool decimalSolde = decimal.TryParse(val[2], out decimal Solde);
                     
-                    val = line.Split(';');
-                   
-                        bool entierKey = int.TryParse(val[0], out int output);
-                        bool entierValue = int.TryParse(val[1], out int output);
-                        if (entierKey && entierValue)
+                    if (entierNum && decimalSolde || val[2] == string.Empty)
+                    {
+                        bool CompteExistant = cpts.Any(c => c.num == NumCpt);
+                        if (!CompteExistant && NumCpt > 0)
                         {
-                            cpts.Add(int.Parse(val[0]), int.Parse(val[1]));
+                            Comptes compte = new Comptes();
+                            cpts.Add(compte);
+                            compte.num = NumCpt;
+                            compte.solde = Solde;
                         }
                         else
                         {
+                            ligne = sr.ReadLine();
                             continue;
                         }
-                    
-                    line = sr.ReadLine();
-                }
-                foreach (KeyValuePair<int,int> kv in cpts)
-                {
-                    Console.WriteLine(kv.Key + ";" + kv.Value);
+                    }
+                    ligne = sr.ReadLine();
                 }
             }
-            Console.WriteLine("Fichier Transactions");
-            using (StreamReader sr = new StreamReader(transactions))
+
+            //récupération du fichier transactions dans une liste d'objet
+            List<Transactions> ListeTransactions = new List<Transactions>();
+            using (StreamReader sr = new StreamReader(ftransactions))
             {
-                string line = sr.ReadLine();
-                while (line != null)
+                string ligne = sr.ReadLine();
+                string[] val;
+                while (ligne != null)
                 {
-                    Console.WriteLine(line);
-                    line = sr.ReadLine();
+                    val = ligne.Split(';');
+                    Transactions Transaction = new Transactions();
+
+                    bool entier0 = int.TryParse(val[0], out int idTransactions);
+                    bool decimal1 = decimal.TryParse(val[1], out decimal montant);
+                    bool entier2 = int.TryParse(val[2], out int Expediteur);
+                    bool entier3 = int.TryParse(val[3], out int Destinataire);
+
+                    if (entier0)
+                    {
+                        bool TransactionExistante = ListeTransactions.Any(c => c.id == idTransactions);
+                        if (!TransactionExistante && decimal1 && entier2 && entier3 && Destinataire >= 0 && Expediteur >= 0 && montant > 0)
+                        {
+                            ListeTransactions.Add(Transaction);
+                            Transaction.id = idTransactions;
+                            Transaction.montant = montant;
+                            Transaction.expediteur = Expediteur;
+                            Transaction.destinataire = Destinataire;
+                        }
+                        else if (!TransactionExistante)
+                        {
+                            ListeTransactions.Add(Transaction);
+                            Transaction.id = idTransactions;
+                        }
+                        else
+                        {
+                            ligne = sr.ReadLine();
+                            continue;
+                        }
+                    }       
+                    ligne = sr.ReadLine();
                 }
             }
+
+            //traitement des transactions
+            Dictionary<int,string> statuts = new Dictionary<int,string>();
+            foreach (Transactions transac in ListeTransactions)
+            {
+                int idt = transac.id;
+                decimal mtn = transac.montant;
+                int exp = transac.expediteur;
+                int des = transac.destinataire;
+
+                if (exp > 0 && des > 0)
+                {
+                    foreach (Comptes c in cpts)
+                    {
+                    bool CompteExped = cpts.Any(e => e.num == exp);
+                    bool CompteDesti = cpts.Any(d => d.num == des);
+                        if (CompteExped && CompteDesti)
+                        {
+                            Comptes CExp = cpts.Find(e => e.num == exp);
+                            Comptes CDest = cpts.Find(d => d.num == des);
+                            if (Transactions.Virement(mtn, CExp, CDest) && exp != des)
+                            {
+                                statuts.Add(idt, "OK");
+                                break;
+                            }
+                            else
+                            {
+                                statuts.Add(idt, "KO");
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            statuts.Add(idt, "KO");
+                            break;
+                        }
+                    }
+                }
+                if (exp == 0 && des > 0)
+                {
+                    foreach (Comptes c in cpts)
+                    {
+                        bool CompteDesti = cpts.Any(d => d.num == des);
+                        if (CompteDesti)
+                        {
+                            Comptes CDest = cpts.Find(d => d.num == des);
+                            Transactions.Dépot(mtn, CDest);
+                            statuts.Add(idt, "OK");
+                            break;
+                        }
+                        else
+                        {
+                            statuts.Add(idt, "KO");
+                            break;
+                        }
+                    }
+                }
+                if (exp > 0 && des == 0)
+                {
+                    foreach (Comptes c in cpts)
+                    {
+                        bool CompteExped = cpts.Any(e => e.num == exp);
+                        if (CompteExped)
+                        {
+                            Comptes CExp = cpts.Find(e => e.num == exp);
+                            if (Transactions.Retrait(mtn, CExp))
+                            {
+                                statuts.Add(idt, "OK");
+                                break;
+                            }
+                            else
+                            {
+                                statuts.Add(idt, "KO");
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            statuts.Add(idt, "KO");
+                            break;
+                        }
+                    }
+                }
+                if (exp == 0 && des == 0)
+                {
+                    statuts.Add(idt, "KO");
+                }
+
+            }
+
+            //écriture des resultats dans le fichiers de sortie
             Console.WriteLine("Fichier Statuts");
-            using (StreamReader sr = new StreamReader(status))
+            using (StreamWriter sw = new StreamWriter(fstatuts))
             {
-                string line = sr.ReadLine();
-                while (line != null)
+                foreach(KeyValuePair<int,string> ligne in statuts)
                 {
-                    Console.WriteLine(line);
-                    line = sr.ReadLine();
+                    sw.WriteLine($"{ligne.Key};{ligne.Value}");
                 }
             }
+            Console.WriteLine(Comptes.NombreComptes);
+            Console.WriteLine(Transactions.NombreTrans);
             Console.ReadLine();
         }
     }
---
+++ Projet I/Transactions.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_I
+{
+    public class Transactions
+    {
+        public int id { get; set; }
+        public DateTime DateEffet { get; set; }
+        public decimal montant { get; set; }
+        public int expediteur { get; set; }
+        public int destinataire { get; set; }
+        public static int NombreTrans { get;set; }
+
+        public Transactions()
+        {
+            id = 0;
+            montant = 0; 
+            expediteur = 0;
+            destinataire = 0;
+            NombreTrans++;
+        }
+
+        public static void Dépot(decimal montant, Comptes CDes)
+        {
+            CDes.solde += montant;
+        }
+
+        public static bool Retrait(decimal montant, Comptes CExp)
+        {
+            if (VerifSolde(montant, CExp))
+            {
+                CExp.solde -= montant;
+                return true;
+            }
+            return false;
+        }
+
+        public static bool Virement(decimal montant, Comptes CExp, Comptes CDes)
+        {
+            if (VerifSolde(montant,CExp))
+            {
+                CExp.solde -= montant;
+                CDes.solde += montant;
+                return true;
+            }
+            return false;
+            
+        }
+
+        public static bool VerifSolde(decimal montant, Comptes CExp)
+        {
+            return CExp.solde > montant;
+        }
+            
+    }
+ 
+}

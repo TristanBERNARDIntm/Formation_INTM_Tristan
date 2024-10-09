2dc95d9b5345f81baab6fc8f6c306fbaa3644aab
Tristan <tristanbernardpro@gmail.com>
2024-10-09 11:52:05 +0200

    test 09/10/24 11h50

--- Projet II/Program.cs
+++ Projet II/Program.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace Projet_II
 {
     internal class Program
-    {
+    {//test
         static void Main(string[] args)
         {
             //chemin d'accès fichiers
